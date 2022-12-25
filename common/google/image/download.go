723416ac7c522bbdb44c404514ceda8221b2ba4d
Hung Nguyen Xuan Pham <hung0913208@gmail.com>
2022-12-26 06:24:23 +0700

    [DRAFT] Integrate with Google search webpage
    Signed-off-by: Hung Nguyen Xuan Pham <hung0913208@gmail.com>

--- common/google/image/download.go
+++ common/google/image/download.go
@@ -2,6 +2,9 @@ package image
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"net/http"
 )
 
 type DownloadImages interface {
@@ -17,13 +20,42 @@ func (self *downloadImagesImpl) downloadSingleImage(url string) ([]byte, error) 
 }
 
 func (self *downloadImagesImpl) searchImageFromGoogle(keyword string) ([]string, error) {
-	// @TODO: please implement search image from google function
+	loadGoogleSearchResult(keyword)
 	return nil, errors.New("")
 }
 
 func (self *downloadImagesImpl) Bulk(keyword string) (int, error) {
 	// @TODO: please implement bulk download function
 	return 0, errors.New("Not implemented")
+}
+
+func loadGoogleSearchResult(searchQuery string) (string, error) {
+	client := http.DefaultClient
+	req, _ := http.NewRequest("GET", searchQuery, nil)
+
+	// @NOTE: No idea why this works, but Google renders the page differently
+	//        with this header. Credit to joeclinton1 on Github for this
+	req.Header.Set("User-Agent",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) "+
+			"AppleWebKit/537.36 (KHTML, like Gecko) "+
+			"Chrome/88.0.4324.104 Safari/537.36")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	htmlPage, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(htmlPage), nil
+}
+
+func buildGoogleSearchQuery(keyword string) string {
+	return fmt.Sprintf("https://www.google.com/search?tbm=isch&q=%s", keyword)
 }
 
 func New() DownloadImages {
